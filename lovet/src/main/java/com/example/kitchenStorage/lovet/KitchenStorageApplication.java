480f7f1e0cb43096398f95ec71d92a7a34a0fd2d
Lovet <lovetetongwe@gmail.com>
2024-11-25 08:55:41 +0000

    Creation of Files and Dependencies

---
+++ lovet/src/main/java/com/example/kitchenStorage/lovet/KitchenStorageApplication.java
@@ -0,0 +1,13 @@
+package com.example.kitchenStorage.lovet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitchenStorageApplication.class, args);
+	}
+
+}
---
+++ lovet/src/test/java/com/example/kitchenStorage/lovet/KitchenStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kitchenStorage.lovet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchenStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
