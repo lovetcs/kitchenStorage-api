901f569d28cb47ce3bf4ddbaf973ae24c90ad81b
Lovet <lovetetongwe@gmail.com>
2024-11-25 09:25:51 +0000

    Service Implementation ``

--- lovet/src/main/java/com/example/kitchenStorage/lovet/ksService.java
+++ lovet/src/main/java/com/example/kitchenStorage/lovet/ksService.java
@@ -1,4 +1,51 @@
 package com.example.kitchenStorage.lovet;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+
+@Service
 public class ksService {
+
+    @Autowired
+    private ksRepo ksRepo;
+
+    public List<ksEntity> getAllItems() {
+        return ksRepo.findAll();
+    }
+
+    public void addItem(ksEntity item) {
+        ksRepo.save(item);
+    }
+
+    public void removeItem(String id) {
+
+        ksEntity item = ksRepo.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Invalid Item"));
+
+        ksRepo.delete(item);
+    }
+
+    public ksEntity getItem(String id) {
+
+        ksEntity item = ksRepo.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Invalid Item"));
+
+        return item;
+    }
+
+    public void updateItem(String id, ksEntity item) {
+
+        ksEntity Olditem = ksRepo.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Invalid Item"));
+
+        ksRepo.save(item);
+
+    }
+}
+
+
+
 }
