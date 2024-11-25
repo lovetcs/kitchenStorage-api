239617d497ada964f064bd6b98e12ff08dd71581
Lovet <lovetetongwe@gmail.com>
2024-11-25 09:32:55 +0000

    Entity Implementation

--- lovet/src/main/java/com/example/kitchenStorage/lovet/ksEntity.java
+++ lovet/src/main/java/com/example/kitchenStorage/lovet/ksEntity.java
@@ -1,4 +1,27 @@
 package com.example.kitchenStorage.lovet;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "items")
+
+
 public class ksEntity {
+    @Id
+    private String id;
+    private String Category;
+    private Integer quantity;
+    private LocalDate expirationDate;
+
 }
