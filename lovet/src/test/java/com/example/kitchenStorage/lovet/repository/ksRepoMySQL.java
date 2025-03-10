b84b4208c43523182ebf3c429626fb65db6f05bf
Lovet <lovetetongwe@gmail.com>
2025-03-10 14:00:26 +0000

    Storing previous mysql repo interface for future switch integration

---
+++ lovet/src/test/java/com/example/kitchenStorage/lovet/repository/ksRepoMySQL.java
@@ -0,0 +1,7 @@
+package com.example.kitchenStorage.lovet.repository;
+
+import com.example.kitchenStorage.lovet.ksEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ksRepoMySQL extends JpaRepository<ksEntity, String> {
+}
